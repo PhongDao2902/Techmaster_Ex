1b35bd5e8e73ed0bef63cf77a62f02a65e63bd0b
PhongDao2902 <46843528+PhongDao2902@users.noreply.github.com>
2021-12-31 11:52:05 +0700

    Delete JSON directory

--- JSON/Main.java
+++
@@ -1,8 +0,0 @@
-package Exercises.JSON;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        ReadFile readFile = new ReadFile();
-        readFile.getData();
-    }
-}
